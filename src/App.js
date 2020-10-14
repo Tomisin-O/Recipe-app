963cd2f2eb7aecac21ab23d6ddd53e43ef501fd0
Tomisin-O <onasanyatomisin@gmail.com>
2020-10-14 11:27:54 +0100

    first commit with css app styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Axios from "axios";
+import "./App.css";
+
+
 
 function App() {
+
+  
+  const APP_ID = "165be659";
+  const APP_KEY = "66153f4a87d0c67abcf2a11be725d17c	";
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+ 
+ const getData = async() => {
+  const result = await Axios.get(url);
+
+  console.log(result);
+
+ };
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 onClick={getData}>Food Searching App</h1>
     </div>
   );
-}
+};
 
-export default App;
+export default App
